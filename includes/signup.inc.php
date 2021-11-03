04eecc901e05ae47d335e565aa7ff2f92e6400d5
Shahnaz Sarker <shahnaz.sarker@gmail.com>
2021-11-03 11:05:18 +0600

    includes file modified

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,25 +1,54 @@
 <?php
+//checks if the submit button is clicked
+    if (isset($_POST['submit'])) {
+        //includes database connection
+        include_once 'dbh.inc.php';
+        //get data from the sign up form
+        $first = $_POST["fname"];
+        $last = $_POST["lname"];
+        $email = $_POST["email"];
+        $uid = $_POST["uid"];
+        $pwd = $_POST["pwd"];
 
-include_once 'dbh.inc.php';
-if (isset($conn)) {
-    $first = mysqli_real_escape_string($conn , $_POST["fname"]);
-    $last = mysqli_real_escape_string($conn, $_POST["lname"]);
-    $email = mysqli_real_escape_string($conn, $_POST["email"]);
-    $uid = mysqli_real_escape_string($conn, $_POST["uid"]);
-    $pwd = mysqli_real_escape_string($conn, $_POST["pwd"]);
+        //check if inputs are empty
+        if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
+            header("Location: ../index.php?signup=empty");
+            exit();
+        } else {
+            if (!preg_match("/^[a-zA-z]*$/", $first) || !preg_match("/^[a-zA-z]*$/", $last)) {
+                header("Location: ../index.php?signup=char");
+                exit();
+            } else {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    header("Location: ../index.php?signup=email");
+                    exit();
+                } else {
+                    header("Location: ../index.php?signup=success");
+                    exit();
+                }
+
+            }
+        }
+    }
+    else{
+            header("Location: ../index.php");
+            exit();
+        }
+
 
     //print_r([$first, $last, $email, $uid, $pwd]);
     //die();
 
-    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare( $stmt, $sql )) {
-        echo "SQL Error!";
-    } else {
-        mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
-        mysqli_stmt_execute($stmt);
-    }
-}
-//mysqli_query($conn, $sql);
-header("Location: ../dataInsert.php?signup=success");
-?>
+//    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
+//    $stmt = mysqli_stmt_init($conn);
+//    if (!mysqli_stmt_prepare( $stmt, $sql )) {
+//        echo "SQL Error!";
+//    } else {
+//        mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+//        mysqli_stmt_execute($stmt);
+//    }
+//}
+////mysqli_query($conn, $sql);
+//header("Location: ../dataInsert.php?signup=success");
+//
+
